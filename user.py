6b4342f10da4b9c7fd53edb25aee219190eeba22
shivamgup0001 <shivamgup00001@gmail.com>
2024-04-26 01:43:35 +0530

    ADDED VALIDATE TRANSACTION FILE

---
+++ user.py
@@ -0,0 +1,26 @@
+import random
+import ecdsa
+
+class User:
+    def __init__(self, name, initial_money):
+        self.name = name
+        self.initial_money = initial_money
+        self.private_key = self.generate_private_key()
+        self.public_key = self.generate_public_key()
+    
+    def generate_private_key(self):
+        # Generate a random private key
+        return ecdsa.SigningKey.generate(curve=ecdsa.SECP256k1)
+    
+    def generate_public_key(self):
+        # Generate a public key based on the private key (this can be more complex in real scenarios)
+        return self.private_key.verifying_key.to_string()
+    
+    def get_money(self):
+        return self.initial_money
+    
+    def get_private_key(self):
+        return self.private_key
+    
+    def get_public_key(self):
+        return self.public_key
